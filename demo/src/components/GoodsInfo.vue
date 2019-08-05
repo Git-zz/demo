b944fea25770b3bd5c6775a0d5ff7ca3b253e3b3
Git-zz <zbt980309@gmail.com>
2019-08-05 14:53:02 +0800

    加入购物车

--- demo/src/components/GoodsInfo.vue
+++ demo/src/components/GoodsInfo.vue
@@ -1,5 +1,14 @@
 <template>
     <div class="goodsinfo">
+<!--      <transition @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter">-->
+<!--      <div class="ball" v-show="ballFlag" ref="ball"></div>&lt;!&ndash;小球动画&ndash;&gt;-->
+<!--      </transition>-->
+      <transition
+        @before-enter="beforeEnter"
+        @enter="enter"
+        @after-enter="afterEnter">
+        <div class="ball" v-show="ballFlag" ref="ball"></div>
+      </transition>
 <!--      轮播图-->
       <div class="mui-card">
         <div class="mui-card-content">
@@ -16,10 +25,10 @@
             <p class="price">
               市场价：<del>￥{{goodsinfo.market_price}}</del>&nbsp;&nbsp;销售价：<span class="now_price">￥{{goodsinfo.sell_price}}</span>
             </p>
-            <p>购买数量：<Goodsinfo_Numbox></Goodsinfo_Numbox></p>
+            <p>购买数量：<Goodsinfo_Numbox @getcount="getSelectedCount" :max="goodsinfo.stock_quantity"></Goodsinfo_Numbox></p>
             <p>
               <mt-button type="primary" size="small">立即购买</mt-button>
-              <mt-button type="danger" size="small">加入购物车</mt-button>
+              <mt-button type="danger" size="small" @click="addToShopCar">加入购物车</mt-button>
             </p>
           </div>
         </div>
@@ -52,7 +61,9 @@
         return{
           id:this.$route.params.id,
           lunbotu:[],
-          goodsinfo:{}
+          goodsinfo:{},
+          ballFlag:false,
+          selectedCount:1
         }
       },
       created(){
@@ -82,6 +93,43 @@
         },
         goComment(id){
           this.$router.push({ name:"GoodsComment",params:{ id } })
+        },
+        addToShopCar(){
+            this.ballFlag=!this.ballFlag
+        },
+        beforeEnter(el) {
+          el.style.transform = 'translate(0, 0)';
+        },
+        enter(el, done) {
+          el.offsetWidth;
+
+          // 小球动画优化思路：
+          // 1. 先分析导致 动画 不准确的 本质原因： 我们把 小球 最终 位移到的 位置，已经局限在了某一分辨率下的 滚动条未滚动的情况下；
+          // 2. 只要分辨率和 测试的时候不一样，或者 滚动条有一定的滚动距离之后， 问题就出现了；
+          // 3. 因此，我们经过分析，得到结论： 不能把 位置的 横纵坐标 直接写死了，而是应该 根据不同情况，动态计算这个坐标值；
+          // 4. 经过分析，得出解题思路： 先得到 徽标的 横纵 坐标，再得到 小球的 横纵坐标，然后 让 y 值 求差， x 值也求 差，得到 的结果，就是横纵坐标要位移的距离
+          // 5. 如何 获取 徽标和小球的 位置？？？   domObject.getBoundingClientRect()
+
+          // 获取小球的 在页面中的位置
+          const ballPosition = this.$refs.ball.getBoundingClientRect();
+          // 获取 徽标 在页面中的位置
+          const badgePosition = document
+            .getElementById("badge")
+            .getBoundingClientRect();
+
+          const xDist = badgePosition.left - ballPosition.left;
+          const yDist = badgePosition.top - ballPosition.top;
+
+          el.style.transform = `translate(${xDist}px, ${yDist}px)`;
+          el.style.transition = 'all 0.5s cubic-bezier(.4,-0.3,1,.68)';
+          done();
+        },
+        afterEnter(el) {
+          this.ballFlag = !this.ballFlag;
+        },
+        getSelectedCount(count){
+            this.selectedCount=count;
+            console.log(this.selectedCount)
         }
       },
       components:{
@@ -106,5 +154,16 @@
     button{
       margin: 15px 0;
     }
+  }
+  .ball{
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    background-color: red;
+    position: absolute;
+    z-index: 99;
+    top: 390px;
+    left: 146px;
+    /*transform: translate(93px,230px);*/
   }
 </style>
--- demo/src/components/Goodsinfo_Numbox.vue
+++ demo/src/components/Goodsinfo_Numbox.vue
@@ -1,7 +1,7 @@
 <template>
-      <div class="mui-numbox" data-numbox-min='1' data-numbox-max='9'>
+     <div class="mui-numbox" data-numbox-min='1' ><!-- :data-numbox-max='max'-->
         <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
-        <input id="test" class="mui-input-numbox" type="number" value="1" />
+        <input id="test" class="mui-input-numbox" type="number" value="1" ref="numbox" @change="countChanged"/>
         <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
       </div>
 </template>
@@ -12,7 +12,19 @@
         name: "Goodsinfo_Numbox",
       mounted() {
           mui(".mui-numbox").numbox();//numbox初始化才能点击按钮
+      },
+      methods:{
+          countChanged(){
+            this.$emit("getcount",parseInt(this.$refs.numbox.value))
+          }
+      },
+      props:["max"],
+      watch:{
+          max:function (newVal,oldVal) {
+            mui(".mui-numbox").numbox().setOption("max",newVal)  //设置最大值。监听到值一改变就触发父组件方法，在控制台输出改变后的值
+          }
       }
+
     }
 </script>
 
